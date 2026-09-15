33047aed76845d2d87f6592953ff1da8e7d17a0b
AYUSH PRADIP KAWANE <167787552+Ayushkawane05@users.noreply.github.com>
2026-09-15 12:25:40 +0530

    Time: 2063 ms (5.03%) | Memory: 42.8 MB (89.95%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,22 +1,29 @@
 class Solution {
     public int maxPalindromes(String s, int k) {
-        int n = s.length();
         if (k == 1)
-            return n;
-        int res = 0;
-        for (int i = 0; i <= n - k; i++) {
-            if (check(s, i, i + k - 1)) {
-                res++;
-                i = i + k - 1;
-            } else if (i < n - k && check(s, i, i + k)) {
-                res++;
-                i += k;
-            }
-        }
-        return res;
+            return s.length();
+        int dp[] = new int[s.length()];
+        Arrays.fill(dp, -1);
+        return check(s, 0, k, dp);
     }
 
-    public boolean check(String s, int i, int j) {
+    public int check(String s, int i, int k, int dp[]) {
+        if (i >= s.length())
+            return 0;
+
+        if (dp[i] != -1)
+            return dp[i];
+        int ans = check(s, i + 1, k, dp);
+
+        for (int j = i + k - 1; j < s.length(); j++) {
+            if (ispal(s, i, j)) {
+                ans = Math.max(ans, 1 + check(s, j + 1, k, dp));
+            }
+        }
+        return dp[i] = ans;
+    }
+
+    public boolean ispal(String s, int i, int j) {
         while (i <= j) {
             if (s.charAt(i++) != s.charAt(j--))
                 return false;
