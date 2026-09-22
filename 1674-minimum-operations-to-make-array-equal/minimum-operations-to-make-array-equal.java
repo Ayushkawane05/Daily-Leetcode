17989676c4ed8c646189b2051c91dc11c9719a4b
AYUSH PRADIP KAWANE <167787552+Ayushkawane05@users.noreply.github.com>
2026-09-22 12:23:49 +0530

    Time: 1 ms (52.00%) | Memory: 42.4 MB (34.06%) - LeetSync

---
+++ 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(int n) {
+        int ans = 0;
+        if (n % 2 != 0) {
+            for (int i = 0; i <= n / 2; i++) {
+                ans = ans + ( i * 2);
+            }
+        } else {
+            for (int i = 0; i < n / 2; i++) {
+                ans = ans + ((i * 2) + 1);
+            }
+        }
+        return ans;
+    }
+}
